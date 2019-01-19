e1046b1b0da3e142b9ea163cf75b1b77031dc5e2
Grigorii Horos <horosgrisa@gmail.com>
2019-01-19 12:57:51 +0200

    Remove unused functions

--- clipboard.plugin.zsh
+++ clipboard.plugin.zsh
@@ -5,16 +5,35 @@ DEPENDENCES_DEBIAN+=(xclip)
 
 if [[ "$OSTYPE" == darwin* ]]; then
   alias o='open'
+  function clip(){
+    if [[ ! -t 0 ]]; then
+        pbcopy
+      else
+        pbpaste
+    fi
+  }
 elif [[ "$OSTYPE" == cygwin* ]]; then
   alias open='cygstart'
   alias o='cygstart'
   alias pbcopy='tee > /dev/clipboard'
   alias pbpaste='cat /dev/clipboard'
+  function clip(){
+    if [[ ! -t 0 ]]; then
+      tee > /dev/clipboard
+    else
+      cat /dev/clipboard
+    fi
+  }
 else
   alias open='xdg-open'
   alias o='xdg-open'
-  if (( $+commands[xclip] )); then
-    alias pbcopy='xclip -selection clipboard -in'
-    alias pbpaste='xclip -selection clipboard -out'
-  fi
+  alias pbcopy='xclip -selection clipboard -in'
+  alias pbpaste='xclip -selection clipboard -out'
+  function clip(){
+    if [[ ! -t 0 ]]; then
+        xclip -selection clipboard -in
+      else
+        xclip -selection clipboard -out
+    fi
+  }
 fi
