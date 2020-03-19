c9f9558206ed7f17470412bfb0ed009a5990e18f
Grigorii Horos <horosgrisa@gmail.com>
2020-03-19 23:56:58 +0200

    Optimize plugin

--- clipboard.plugin.zsh
+++ clipboard.plugin.zsh
@@ -1,58 +1,14 @@
 #!/usr/bin/env zsh
+# Standarized ZSH polyfills, following:
+# https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+if [[ $PMSPEC != *f* ]] {
+  fpath+=( "${0:h}/functions" )
+}
+
+autoload -Uz open pbcopy pbpaste clip
 
 DEPENDENCES_ARCH+=(xclip)
 DEPENDENCES_DEBIAN+=(xclip)
-
-if (( $+commands[xdg-open] )); then
-  alias open='xdg-open'
-  alias o='xdg-open'
-  alias pbcopy='xclip -selection clipboard -in'
-  alias pbpaste='xclip -selection clipboard -out'
-  function clip() {
-    if [[ ! -t 0 ]]; then
-      xclip -selection clipboard -in
-    else
-      xclip -selection clipboard -out
-    fi
-  }
-elif [[ "$OSTYPE" == darwin* ]]; then
-  alias o='open'
-  function clip() {
-    if [[ ! -t 0 ]]; then
-      pbcopy
-    else
-      pbpaste
-    fi
-  }
-elif [[ "$OSTYPE" == (cygwin*|msys) ]]; then
-  if [[ $OSTYPE == cygwin* ]]; then
-    alias open='cygstart'
-    alias o='cygstart'
-  else
-    alias open='explorer'
-    alias o='explorer'
-  fi
-  alias pbcopy='tee > /dev/clipboard'
-  alias pbpaste='cat /dev/clipboard'
-  function clip() {
-    if [[ ! -t 0 ]]; then
-      tee > /dev/clipboard
-    else
-      cat /dev/clipboard
-    fi
-  }
-  # WSL
-elif [[ $OSTYPE == linux* ]] && [[ -r /proc/version ]] && [[ $(< /proc/version) == *Microsoft* ]] \
-  && ! xclip -o &> /dev/null; then
-  alias open='explorer.exe'
-  alias o='explorer.exe'
-  alias pbcopy='clip.exe'
-  alias pbpaste='powershell.exe -Command Get-Clipboard'
-  function clip() {
-    if [[ ! -t 0   ]]; then
-      clip.exe
-    else
-      powershell.exe -Command Get-Clipboard
-    fi
-  }
-fi
