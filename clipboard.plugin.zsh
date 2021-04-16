6f44d9eed6af79a02c77bb4997e3036b03fde03c
Grigorii Horos <horosgrisa@gmail.com>
2021-04-16 23:30:58 +0300

    Fixes

--- clipboard.plugin.zsh
+++ clipboard.plugin.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 # Standarized ZSH polyfills, following:
 # https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
-0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
 if [[ $PMSPEC != *f* ]] {
